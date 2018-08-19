cb3d25cf4d12c324fb7e276d9e81d4035b765632
Don McLamb <mclamb.donald@gmail.com>
2018-08-19 00:32:30 -0400

    finished but needs refactoring and tests

--- clicks.rb
+++ clicks.rb
@@ -1,12 +1,17 @@
 require 'pry-nav'
 require 'pry-remote'
 require 'pp'
+require 'active_support/time'
+require 'byebug'
 
 @clicks = [
 { ip:'22.22.22.22', timestamp:'3/11/2016 02:02:58', amount: 7.00 },
 { ip:'11.11.11.11', timestamp:'3/11/2016 02:12:32', amount: 6.50 },
 { ip:'11.11.11.11', timestamp:'3/11/2016 02:13:11', amount: 7.25 },
+{ ip:'11.11.11.11', timestamp:'3/11/2016 02:12:11', amount: 7.25 },
+{ ip:'44.44.44.44', timestamp:'3/11/2016 02:13:54', amount: 8.74 },
 { ip:'44.44.44.44', timestamp:'3/11/2016 02:13:54', amount: 8.75 },
+{ ip:'44.44.44.44', timestamp:'3/11/2016 02:13:54', amount: 8.76 },
 { ip:'22.22.22.22', timestamp:'3/11/2016 05:02:45', amount: 11.00 },
 { ip:'44.44.44.44', timestamp:'3/11/2016 06:32:42', amount: 5.00 },
 { ip:'22.22.22.22', timestamp:'3/11/2016 06:35:12', amount: 2.00 },
@@ -43,7 +48,30 @@ require 'pp'
 def get_most_expensive_click
 end
 
-def get_clicks_with_in_period
+def get_clicks_with_in_period(clicks)
+    time_range_obj = {}
+    clicks.each_with_index do |click, index|
+
+        time_of_click = DateTime.parse(click[:timestamp])
+        start_of_time_block = time_of_click.beginning_of_hour
+        end_of_time_block = time_of_click.beginning_of_hour + 59.minutes
+
+        start_time_string = start_of_time_block.strftime
+        end_time_string = end_of_time_block.strftime
+
+  
+        time_range_obj[start_time_string] ||= Array.new
+ 
+        time_range_obj[end_time_string] ||= Array.new
+
+        time_of_click >= start_of_time_block ? time_range_obj[start_time_string].push(click) : time_range_obj[end_time_string].push(click)
+    end
+    time_range_obj
+
+
+    # //iterate through time by the hour
+    # //see if the current click event is with in the current hour
+    # //if it is add that hour gap and event to the has and the click it self as the value in an array of objects
 end
 
 def filter_duplicates_from_period
@@ -59,8 +87,8 @@ def filter_for_bot_clicks(clicks)
     # I added a sort here because I noticed that this was filter for delete was not deleting all the instances of 55.55.55.55, it seemed to be getting the indices wrong
     # sorting resolved this
     clicks.sort{ |click_event1, click_event2| click_event1["ip"] <=> click_event2["ip"] }.each_with_index do |item, index|
-    click_count.push(item[:ip])
-    current_ip = item[:ip]
+        click_count.push(item[:ip])
+        current_ip = item[:ip]
         if click_count.select{|ip| ip == item[:ip]}.count >= 10
             clicks.delete_if{ |x| x[:ip] == current_ip} 
         end
@@ -69,4 +97,24 @@ def filter_for_bot_clicks(clicks)
 end
 
 
-pp filter_for_bot_clicks(@clicks)
+@filtered_clicks = filter_for_bot_clicks(@clicks)
+
+@time_obj =  get_clicks_with_in_period(@filtered_clicks)
+
+# @time_obj.each do |t|
+#     t.sort{|click1, click2| click1.}
+# end
+# @time_obj["2016-11-03T02:00:00+00:00"]
+
+# @time_obj["2016-11-03T07:00:00+00:00"].group_by{|y| y[:ip]}.
+# map{|key, value| value.max_by{ |click| click[:amount]}}
+
+@test = []
+highest_values = @time_obj.each do |key, click_obj|
+    # pp click_obj
+    @test.push click_obj.sort{|s,x| s[:timestamp] <=> x[:timestamp]}.group_by{|click_group_ip| click_group_ip[:ip]}.map{|key, value| value.max_by{ |click_for_max| click_for_max[:amount]}}
+
+    #  @test.push click_obj.group_by{|click_group_ip| click_group_ip[:ip]}.map{|key, value| value.max_by{ |click_for_max| click_for_max[:amount]}}
+end
+
+pp @test.flatten
